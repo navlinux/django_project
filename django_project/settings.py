9e49034208d4085871e614ba218dd2c067a8d452
navlinux <2nav.linux@gmail.com>
2022-05-28 13:40:50 +0530

    Updated settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1)_g6)!mn$gx-pczi%r7m6=1(y$w#8(cy%bcug_mv35l4y*88t'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['navlinux-django-blog.herokuapp.com']
 
 
 # Application definition
